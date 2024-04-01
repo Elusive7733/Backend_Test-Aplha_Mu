29650c28611a42b314b5d779903ad5ef27ee3f68
Abdullah Raheel <Abdullaharaheel@gmail.com>
2024-04-01 10:53:06 +0500

    Update app.controller.spec.ts

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   })
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!')
+    it('should return "Backend Test By - Alpha MU"', () => {
+      expect(appController.getHello()).toBe('Backend Test By - Alpha MU')
     })
   })
 })
